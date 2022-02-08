136483e1caa30bf0a53f2d05ec9a1814cd3afd81
karlo <k.koelewijn2@gmail.com>
2022-02-08 01:09:37 +0100

    init commit

---
+++ app/src/main.cpp
@@ -0,0 +1,49 @@
+#include "main.h"
+
+int main() {
+
+    std::shared_ptr<Data> data = std::make_shared<Data>(
+        getFirstLineFromFile("token.tkn"));
+
+    TgBot::Bot botClient(data->Token());
+    
+    botClient.getEvents().onCommand("start", [&botClient](TgBot::Message::Ptr message) {
+        botClient.getApi().sendMessage(message->chat->id, "hi!");
+    });
+    
+    botClient.getEvents().onCommand("quit", [&botClient, &data](TgBot::Message::Ptr message) {
+        data->SetRun(false);
+        botClient.getApi().sendMessage(message->chat->id, "quiting");
+    });
+    
+    botClient.getEvents().onAnyMessage([&botClient](TgBot::Message::Ptr message) {
+        std::cout << "user wrote: " << message->text << "\n";
+        if (StringTools::startsWith(message->text, "/start"))
+        {
+            return;
+        }
+        if (StringTools::startsWith(message->text, "/quit"))
+        {
+            return;
+        }
+        botClient.getApi().sendMessage(message->chat->id, "your message is: " + message->text);
+    });
+    
+    try
+    {
+        std::cout << "bot username: " << botClient.getApi().getMe()->username << "\n";
+        TgBot::TgLongPoll longPoll(botClient);
+        while (data->Run())
+        {
+            std::cout << "long poll started\n";
+            longPoll.start();
+        }
+    }
+    catch (const std::exception& e)
+    {
+        std::cout << "Error: " << e.what() << "\n";
+    }
+
+    return 0;
+}
+
---
+++ app/src/main.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "tgbot/tgbot.h"
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <thread>
+#include <memory>
+
+class Data
+{
+public:
+	Data(const std::string& _tkn) :
+		token(std::string(_tkn)),
+        run(true)
+	{
+	}
+
+public:
+	std::string& Token() { return token; }
+    bool& Run() { return run; }
+    void SetRun(bool value) { run = value; }
+
+private:
+	std::string token;
+    bool run;
+
+};
+
+std::string getFirstLineFromFile(const std::string& fileName) {
+    std::fstream infile(fileName, std::ios::in);
+    std::string line;
+
+    while (std::getline(infile, line))
+    {
+        return line;
+    }
+
+    return "";
+}
